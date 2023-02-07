902b8171bd1261565c5f17ab56c175deecba48cc
Stanislaw <stasmohoid228337@gmil.com>
2023-02-07 14:50:59 +0200

    added login with token

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,33 @@
+import { RouterProvider } from 'react-router';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
+import router from '../../routes/router';
+import { accountFetching, accountFetched, accountFetchingError } from '../Login/loginSlice';
+
 import './app.scss';
-import PatientLogin from '../Login/PatientLogin/PatientLogin';
+
 const App = () => {
+
+	const dispatch = useDispatch();
+	const token = localStorage.getItem('token');
+	if(token !== 'undefined') {
+		dispatch(accountFetching());
+        axios.post('/auth/login', {}, {
+			headers: {
+				'Authorization': token
+			}
+		})
+        .then(res => {
+            dispatch(accountFetched(res.data));
+            localStorage.setItem('token', res.data.token);
+        })
+        .catch(() => dispatch(accountFetchingError()));
+	}
 
 	return (
 		<div className="app">
-			<PatientLogin/>
+			<RouterProvider router={router} />
 		</div>
 	)
 }
